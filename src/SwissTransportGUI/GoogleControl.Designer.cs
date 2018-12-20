97f9b4379a83e695b3162f5034cc88cd718036ff
ggthis272 <marcorocchinotti00@gmail.com>
2018-12-20 16:16:22 +0100

    Google Maps Funktion

--- src/SwissTransportGUI/GoogleControl.Designer.cs
+++ src/SwissTransportGUI/GoogleControl.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace SwissTransportGUI
+﻿using System;
+
+namespace SwissTransportGUI
 {
     partial class GoogleControl
     {
@@ -31,35 +33,28 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GoogleControl));
             this.button1 = new System.Windows.Forms.Button();
             this.TXT_station = new System.Windows.Forms.TextBox();
-            this.LBX_station = new System.Windows.Forms.ListBox();
             this.WEB_google_maps = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LBX_station = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(305, 63);
+            this.button1.Location = new System.Drawing.Point(198, 75);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Suchen";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // TXT_station
             // 
-            this.TXT_station.Location = new System.Drawing.Point(89, 63);
+            this.TXT_station.Location = new System.Drawing.Point(26, 75);
             this.TXT_station.Name = "TXT_station";
             this.TXT_station.Size = new System.Drawing.Size(166, 20);
             this.TXT_station.TabIndex = 1;
             this.TXT_station.TextChanged += new System.EventHandler(this.TXT_station_TextChanged);
-            // 
-            // LBX_station
-            // 
-            this.LBX_station.FormattingEnabled = true;
-            this.LBX_station.Location = new System.Drawing.Point(89, 80);
-            this.LBX_station.Name = "LBX_station";
-            this.LBX_station.Size = new System.Drawing.Size(166, 95);
-            this.LBX_station.TabIndex = 2;
             // 
             // WEB_google_maps
             // 
@@ -69,11 +64,32 @@
             this.WEB_google_maps.Size = new System.Drawing.Size(611, 315);
             this.WEB_google_maps.TabIndex = 3;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Google Maps";
+            // 
+            // LBX_station
+            // 
+            this.LBX_station.FormattingEnabled = true;
+            this.LBX_station.Location = new System.Drawing.Point(26, 101);
+            this.LBX_station.Name = "LBX_station";
+            this.LBX_station.Size = new System.Drawing.Size(166, 95);
+            this.LBX_station.TabIndex = 2;
+            this.LBX_station.SelectedIndexChanged += new System.EventHandler(this.LBX_station_SelectedIndexChanged);
+            // 
             // GoogleControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.WEB_google_maps);
             this.Controls.Add(this.LBX_station);
             this.Controls.Add(this.TXT_station);
@@ -85,11 +101,17 @@
 
         }
 
+        private void LBX_station_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
         #endregion
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox TXT_station;
-        private System.Windows.Forms.ListBox LBX_station;
         private System.Windows.Forms.WebBrowser WEB_google_maps;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox LBX_station;
     }
 }
